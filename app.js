c0e2ab8c0a8b03981897f9675ba676e20173f2d4
Ilya Petrenko <gr0x562@gmail.com>
2020-05-02 18:03:07 +0300

    add creation contact

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const CONTACTS = [
   {id: 1, name: 'Илья', value: '+7(978)034-89-37', marked: false}
 ];
 
+app.use(express.json());
+
 app.get('/api/contacts', (req, res) => {
   res.status(200).json(CONTACTS);
+});
 
+app.post('/api/contacts', (req, res) => {
+  const contact = {...req.body, id: Date.now(), marked: false};
+  CONTACTS.push(contact);
+  res.status(201).json(contact);
 })
 
 
